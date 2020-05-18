4953a758cf9ea7490ec0b2f2e35086c4f4b5d0a0
Alex <sagaidak1337@gmail.com>
2020-05-18 17:28:59 +0300

    Updates

---
+++ precache-manifest.2d348cd590e0cabdf506d267d0b1634b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d0e3f5c4175c2db5823d718e54b77e",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "bd0b2e69c23030402998",
+    "url": "/game/static/css/main.a3df9fb7.chunk.css"
+  },
+  {
+    "revision": "98dbb12f101b6c4289be",
+    "url": "/game/static/js/2.b3d20ce5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/game/static/js/2.b3d20ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd0b2e69c23030402998",
+    "url": "/game/static/js/main.a1856187.chunk.js"
+  },
+  {
+    "revision": "be29b782fb50e251a0ba",
+    "url": "/game/static/js/runtime-main.f238e623.js"
+  }
+]);
